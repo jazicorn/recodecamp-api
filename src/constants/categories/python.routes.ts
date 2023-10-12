ce0841d5dc4246bba166a879f32bfdc1fdf6f2d1
jazicorn <40859840+jazicorn@users.noreply.github.com>
2023-10-12 14:40:12 -0400

    added additional info to routes

--- src/constants/categories/python.routes.ts
+++ src/constants/categories/python.routes.ts
@@ -6,5 +6,14 @@ export const _ROUTES_PYTHON = [
             "singleLine": ['comments/single/all'],
             "multiLine": ['comments/multi/all'],
         },
+        "info": {
+            "sources": [
+                {"w3schools": "https://www.w3schools.com/python/python_comments.asp"},
+            ],
+            "desc": [
+                { "paragraph": "Comments can be used to explain Python code. Comments can be used to make the code more readable. Comments can be used to prevent execution when testing code."},
+            ],
+        },
+        "tags": ["Python", "comments", "singleline", "multiline", "documentation"]
     },
 ]
