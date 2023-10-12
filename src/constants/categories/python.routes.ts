09979fbd8528ba37b27191c628143f3dac359e8c
jazicorn <40859840+jazicorn@users.noreply.github.com>
2023-10-12 15:48:11 -0400

    added routes to api documentation

--- src/constants/categories/python.routes.ts
+++ src/constants/categories/python.routes.ts
@@ -1,19 +1,31 @@
 export const _ROUTES_PYTHON = [
     {
-        "id": 1,
-        "category": ["comments", "comments/all"],
-        "subCategories": {
-            "singleLine": ['comments/single/all'],
-            "multiLine": ['comments/multi/all'],
+        id: 1,
+        category: ['comments', 'comments/all'],
+        subCategories: {
+            singleLine: ['comments/single/all'],
+            multiLine: ['comments/multi/all'],
         },
-        "info": {
-            "sources": [
-                {"w3schools": "https://www.w3schools.com/python/python_comments.asp"},
+        info: {
+            sources: [
+                {
+                    w3schools:
+                        'https://www.w3schools.com/python/python_comments.asp',
+                },
             ],
-            "desc": [
-                { "paragraph": "Comments can be used to explain Python code. Comments can be used to make the code more readable. Comments can be used to prevent execution when testing code."},
+            desc: [
+                {
+                    paragraph:
+                        'Comments can be used to explain Python code. Comments can be used to make the code more readable. Comments can be used to prevent execution when testing code.',
+                },
             ],
         },
-        "tags": ["Python", "comments", "singleline", "multiline", "documentation"]
+        tags: [
+            'Python',
+            'comments',
+            'singleline',
+            'multiline',
+            'documentation',
+        ],
     },
-]
+];
